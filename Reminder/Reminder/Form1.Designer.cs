5978e1ae6b5ea6c432e7af67774f06fced00667e
Sefat Anam <37630292+sefatanam@users.noreply.github.com>
2019-02-26 10:55:27 +0600

    Add files via upload

---
+++ Reminder/Reminder/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace Reminder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dayComboBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.CheckButton = new System.Windows.Forms.Button();
+            this.timeTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dayComboBox
+            // 
+            this.dayComboBox.FormattingEnabled = true;
+            this.dayComboBox.Items.AddRange(new object[] {
+            "----Select----",
+            "SAT",
+            "SUN",
+            "MON",
+            "TUE",
+            "WED",
+            "THU",
+            "FRI"});
+            this.dayComboBox.Location = new System.Drawing.Point(160, 75);
+            this.dayComboBox.Name = "dayComboBox";
+            this.dayComboBox.Size = new System.Drawing.Size(252, 28);
+            this.dayComboBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Date";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(49, 141);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Time";
+            // 
+            // CheckButton
+            // 
+            this.CheckButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CheckButton.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.CheckButton.Location = new System.Drawing.Point(202, 226);
+            this.CheckButton.Name = "CheckButton";
+            this.CheckButton.Size = new System.Drawing.Size(98, 40);
+            this.CheckButton.TabIndex = 3;
+            this.CheckButton.Text = "Check";
+            this.CheckButton.UseVisualStyleBackColor = true;
+            this.CheckButton.Click += new System.EventHandler(this.CheckButton_Click);
+            // 
+            // timeTextBox
+            // 
+            this.timeTextBox.Location = new System.Drawing.Point(164, 141);
+            this.timeTextBox.Name = "timeTextBox";
+            this.timeTextBox.Size = new System.Drawing.Size(247, 26);
+            this.timeTextBox.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(230, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(115, 26);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Reminder";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dayComboBox);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.timeTextBox);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.CheckButton);
+            this.groupBox1.Location = new System.Drawing.Point(58, 122);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(474, 332);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Select Date Time";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("MS Reference Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(189, 68);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(203, 26);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Dhaka New Market";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(620, 526);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label3);
+            this.Name = "Form1";
+            this.Text = "Reminder";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox dayComboBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button CheckButton;
+        private System.Windows.Forms.TextBox timeTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Reminder/Reminder/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Reminder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CheckButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int time = Convert.ToInt32(timeTextBox.Text);
+                string day = dayComboBox.Text;
+
+                if (day == "SAT" || day == "SUN" || day == "MON" || day == "WED" || day == "THU" || day == "FRI" ||
+                   day == "sat" || day == "sun" || day == "mon" || day == "wed" || day == "thu" || day == "fri")
+                {
+                    if (time >= 8 && time <= 20) //here time is 24hour format
+                    {
+                        MessageBox.Show("Market is open now, Close at 8.00 PM\n" +
+                                         "Thanks for check-in Our Market\n\n");
+
+                        dayComboBox.Text = "Select Day from here";
+                        timeTextBox.Text = "";
+                    }
+                    else if (time<8 && time>0)
+                    {
+                        MessageBox.Show("Today Market is open at 8.00 AM \n" +
+                                        "Thanks for check-in Our Market\n\n");
+
+                        dayComboBox.Text = "Select Day from here";
+                        timeTextBox.Text = "";
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("Market is Already Closed\n" +
+                                        " Please,Come Tomorrow at 8.00 AM from 8.00 PM ");
+
+                        dayComboBox.Text = "Select Day from here";
+                        timeTextBox.Text = "";
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Tuesday ! Today is our Holiday\n" +
+                                    "Please Come at Tomorrow before 8.00 PM\nThanks");
+
+                    dayComboBox.Text = "Select Day from here";
+                    timeTextBox.Text = "";
+                }
+
+            }
+            catch (Exception exe)
+            {
+
+                MessageBox.Show(exe.Message);
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dayComboBox.Text = "Select Day from here";
+        }
+    }
+}
